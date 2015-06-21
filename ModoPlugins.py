2695e8b3e42208450adec9b59282266b3c533b82
Alex Widener <alexwidener@gmail.com>
2015-06-20 22:26:32 -0700

    ET PHONE HOME
    ELLIOTTTTT

--- ModoPlugins.py
+++ ModoPlugins.py
@@ -3,10 +3,28 @@
 """
 Sublime Text Plugin for generating Modo Commands inside of Sublime.
 
-Usage:
+Installation:
 Install through Package Control by searching for "Modo Plugin Builder"
+Or download the ZIP and go to Preferences > Browse Packages and put the
+ModoPluginBuilder folder in there. Then restart Sublime.
 
-words
+Usage:
+You will have a new submenu at the bottom of Tools which will allow you to
+create a Modo command or create a new kit.
+
+Both of these go to the USER Scripts folder. Part of the reason I chose to
+make this is that on OS X, the new function that is currently inside of Modo
+will install to the root directory and cause permission errors. The other
+reason I made this is because it's a nice idea but it's only available to
+people who purchased 901.
+
+Plus, I prefer Sublime.
+
+You can also directly open the Modo documentation forums as well as
+search the TDSDK and SDK pages from within Sublime.
+Simple go to Tools > Modo Plugins > Search TDSDK Docs / Search PyAPI Docs and
+the input will appear at the bottom of the Sublime Text window for what you
+want to search for.
 """
 
 __author__ = "Alex Widener"
